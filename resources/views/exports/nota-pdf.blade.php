fce9d29c581e546b40ad76b61e44c5a2e5e52eeb
Ardana Yatra <imadeardanayatra@gmail.com>
2025-06-12 16:06:36 +0800

    fix return

--- resources/views/exports/nota-pdf.blade.php
+++ resources/views/exports/nota-pdf.blade.php
@@ -1,3 +1,4 @@
+{{-- resources/views/nota-pdf.blade.php --}}
 <!DOCTYPE html>
 <html lang="id">
 
@@ -8,13 +9,7 @@
             font-family: monospace;
             font-size: 12px;
             margin: 0;
-            padding: 0;
-        }
-
-        .wrapper {
-            margin: 0 8px;
-            /* tipis kiri & kanan */
-            padding: 10px 0;
+            padding: 5px;
         }
 
         .center {
@@ -26,74 +21,65 @@
         }
 
         hr {
-            border: none;
-            border-top: 1px dashed #333;
-            margin: 4px 0;
+            border: dashed 1px #333;
+            margin: 5px 0;
         }
 
         table {
             width: 100%;
             border-collapse: collapse;
-            margin: 4px 0;
+            margin: 5px 0;
         }
 
         td {
-            padding: 2px 0;
+            padding: 3px 0;
         }
 
-        .header .title {
-            font-size: 14px;
+        .total-row {
+            display: flex;
+            justify-content: space-between;
             font-weight: bold;
-        }
-
-        .header .subtitle {
-            font-size: 10px;
-            color: #555;
-        }
-
-        .total-row span {
-            font-size: 13px;
         }
     </style>
 </head>
 
 <body>
-    <div class="wrapper">
-        <div class="header center">
-            <div class="title">SI PARFUM</div>
-            <div class="subtitle">Jl. Contoh No.123, Jakarta</div>
-            <div class="subtitle">Telp. (021) 1234567</div>
-        </div>
-        <hr />
-        <div>
-            <div>No Nota: <strong>PNJ-{{ $transaksi->id }}</strong></div>
-            <div>Tanggal: {{ $transaksi->tanggal_transaksi->format('d-m-Y H:i') }}</div>
-            <div>Kasir: {{ $transaksi->kasir->username ?? '-' }}</div>
-        </div>
-        <hr />
-        <table>
-            <tr>
-                <td><strong>Barang</strong></td>
-                <td class="right">{{ $transaksi->barang->nama_barang }}</td>
-            </tr>
-            <tr>
-                <td>Harga</td>
-                <td class="right">Rp {{ number_format($transaksi->harga_pokok, 0, ',', '.') }}</td>
-            </tr>
-            <tr>
-                <td>Qty</td>
-                <td class="right">{{ $transaksi->jumlah_penjualan }}</td>
-            </tr>
-            <tr>
-                <td>Subtotal</td>
-                <td class="right">Rp {{ number_format($transaksi->subtotal, 0, ',', '.') }}</td>
-            </tr>
-        </table>
-        <hr />
-        <div class="total-row flex justify-between font-bold">
-            <span>Total Harga:</span>
-            <span class="right">Rp {{ number_format($transaksi->total_harga, 0, ',', '.') }}</span>
-        </div>
+    <div class="center" style="font-weight:bold;">GRYA PARFUM</div>
+    <div class="center">Jln.pasir putih kedonganan kec kute kab badung</div>
+    <div class="center small">Telp. 081234567</div>
+    <hr />
+    <div>No Nota: PNJ-{{ $transaksi->id }}</div>
+    <div>Tanggal: {{ $transaksi->tanggal_transaksi->format('d-m-Y H:i') }}</div>
+    <div>Kasir: {{ $transaksi->kasir->username ?? '-' }}</div>
+    <hr />
+    <table>
+        <tr>
+            <td>Barang</td>
+            <td class="right">{{ $transaksi->barang->nama_barang }}</td>
+        </tr>
+        <tr>
+            <td>Harga</td>
+            <td class="right">Rp {{ number_format($transaksi->harga_pokok, 0, ',', '.') }}</td>
+        </tr>
+        <tr>
+            <td>Qty</td>
+            <td class="right">{{ $transaksi->jumlah_penjualan }}</td>
+        </tr>
+        <tr>
+            <td>Subtotal</td>
+            <td class="right">Rp {{ number_format($transaksi->subtotal, 0, ',', '.') }}</td>
+        </tr>
+        <tr>
+            <td>Pajak ({{ $transaksi->pajak->presentase }}%)</td>
+            <td class="right">
+                Rp {{ number_format($transaksi->total_harga - $transaksi->subtotal, 0, ',', '.') }}
+            </td>
+        </tr>
+    </table>
+    <hr />
+    <div class="total-row">
+        <span>Total Harga:</span>
+        <span class="right">Rp {{ number_format($transaksi->total_harga, 0, ',', '.') }}</span>
     </div>
 </body>
 
