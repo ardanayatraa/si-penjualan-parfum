a108035b5753899dd1cdedda6ff09e2611eb0559
Ardana Yatra <imadeardanayatra@gmail.com>
2025-06-30 15:47:54 +0800

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -123,23 +123,31 @@
                                 <span>User</span>
                             </a>
 
-                            <a href="{{ route('accounting') }}"
-                                class="flex items-center px-3 py-2 text-sm font-medium rounded-lg hover:bg-orange-200 transition-all {{ request()->routeIs('accounting') ? 'sidebar-active' : '' }}">
-                                <i class="fas fa-book w-5 h-5 mr-3"></i>
-                                <span>Accounting</span>
+
+                            <a href="{{ route('pajak-transaksi') }}"
+                                class="flex items-center px-3 py-2 text-sm font-medium rounded-lg hover:bg-orange-200 transition-all {{ request()->routeIs('pajak-transaksi') ? 'sidebar-active' : '' }}">
+                                <i class="fas fa-percent w-5 h-5 mr-3"></i>
+                                <span>Pajak Transaksi</span>
 
 
-                                <a href="{{ route('laporan') }}"
-                                    class="flex items-center px-3 py-2 text-sm font-medium rounded-lg hover:bg-orange-200 transition-all {{ request()->routeIs('laporan') ? 'sidebar-active' : '' }}">
-                                    <i class="fas fa-chart-bar w-5 h-5 mr-3"></i>
-                                    <span>Laporan</span>
-                                </a>
 
-                                <a href="{{ route('grafik.penjualan') }}"
-                                    class="flex items-center px-3 py-2 text-sm font-medium rounded-lg hover:bg-orange-200 transition-all {{ request()->routeIs('grafik.penjualan') ? 'sidebar-active' : '' }}">
-                                    <i class="fas fa-chart-line w-5 h-5 mr-3"></i>
-                                    <span>Grafik Penjualan</span>
-                                </a>
+                                <a href="{{ route('accounting') }}"
+                                    class="flex items-center px-3 py-2 text-sm font-medium rounded-lg hover:bg-orange-200 transition-all {{ request()->routeIs('accounting') ? 'sidebar-active' : '' }}">
+                                    <i class="fas fa-book w-5 h-5 mr-3"></i>
+                                    <span>Accounting</span>
+
+
+                                    <a href="{{ route('laporan') }}"
+                                        class="flex items-center px-3 py-2 text-sm font-medium rounded-lg hover:bg-orange-200 transition-all {{ request()->routeIs('laporan') ? 'sidebar-active' : '' }}">
+                                        <i class="fas fa-chart-bar w-5 h-5 mr-3"></i>
+                                        <span>Laporan</span>
+                                    </a>
+
+                                    <a href="{{ route('grafik.penjualan') }}"
+                                        class="flex items-center px-3 py-2 text-sm font-medium rounded-lg hover:bg-orange-200 transition-all {{ request()->routeIs('grafik.penjualan') ? 'sidebar-active' : '' }}">
+                                        <i class="fas fa-chart-line w-5 h-5 mr-3"></i>
+                                        <span>Grafik Penjualan</span>
+                                    </a>
                         @endif
 
                         {{-- Menu untuk KASIR --}}
