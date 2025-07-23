c1919cd206287cf8ec83b87f1f9f8a2b4a7e9e48
Ardana Yatra <imadeardanayatra@gmail.com>
2025-07-23 18:24:10 +0800

    lala

--- resources/views/components/link-action-penjualan.blade.php
+++ resources/views/components/link-action-penjualan.blade.php
@@ -1,5 +1,3 @@
 <div class="flex gap-2">
     <x-button wire:click="edit({{ $id }})">Edit</x-button>
-    <x-button wire:click="delete({{ $id }})" class="bg-red-500 hover:bg-red-600">Hapus</x-button>
-    <x-button wire:click="createReturn({{ $id }})" class="bg-yellow-500 hover:bg-yellow-600">Return Barang</x-button>
 </div>
